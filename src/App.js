9563d044ae6c0826a84122fe2cb2c1338af871f8
frameshft <adiletkadyrov@gmail.com>
2017-06-14 16:49:36 +0600

    Split Up Components

--- src/App.js
+++ src/App.js
@@ -49,15 +49,41 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <h1>WutFace</h1>
-        <form>
-          <input
-            type='text'
-            onChange={ this.onSearchChange }
-          />
-        </form>
-        { list.filter(isSearched(searchTerm)).map(item =>
-          <div key={item.objectID}>
+        <Search
+          value={ searchTerm }
+          onChange={ this.onSearchChange }
+        />
+        <Table
+          list={ list }
+          pattern={ searchTerm }
+          onDismiss={ this.onDismiss }
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={ value }
+          onChange={ onChange }
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        { list.filter(isSearched(pattern)).map(item => <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
@@ -65,18 +91,11 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button
-                onClick={() => this.onDismiss(item.objectID)}
-                type='button'
-              >
-                Dismiss
-              </button>
+              <button onClick={() => onDismiss(item.objectID)} type="button">Dismiss</button>
             </span>
-          </div>
-        )}
+        </div>)}
       </div>
     );
   }
 }
-
 export default App;
